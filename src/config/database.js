c40c50214a5e5dd6a4ec190b91e47950b42cf488
Sanjay <kumarsanjay3722@gmail.com>
2025-05-29 01:29:25 +0530

    env file added

--- src/config/database.js
+++ src/config/database.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 
 const connectDB = async () => {
-   await  mongoose.connect('mongodb+srv://kumarsanjay3722:8VQ5kby4Pb8KVtSa@cluster0.xrysjwu.mongodb.net/devTinder')
+   await  mongoose.connect(process.env.DATABASE)
 }
 
 module.exports = connectDB;
