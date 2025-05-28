d14f3253f8b229dee8cf5448e0efd012d6e2ef19
Sanjay <kumarsanjay3722@gmail.com>
2025-05-28 15:42:38 +0530

    route added

--- src/app.js
+++ src/app.js
@@ -2,12 +2,38 @@ const express = require('express');
 
 const app = express();
 
-app.use('/test', (req, res) => {
-    res.send('Test from the server!')
+
+app.get('/user', (req, res) => {
+    res.send({name: 'sanjay', mobile: '8527020679'})
 })
-app.use('/hello', (req, res) => {
-    res.send('Hello from the server!')
+
+app.post('/user', (req, res) => {
+    res.send("This is the post call")
 })
-app.listen(3000, () => {
+
+app.delete('/user', (req, res) => {
+    res.send("This is the delete call")
+})
+
+app.patch('/user', (req, res) => {
+    res.send("This is the patch call")
+})
+
+app.put('/user', (req, res) => {
+    res.send("This is the put call")
+})
+
+// app.use('/test', (req, res) => {
+//     res.send('Test from the server!')
+// })
+// app.use('/hello', (req, res) => {
+//     res.send('Hello from the server!')
+// })
+
+// app.use('/', (req, res) => {
+//     console.log("Hello Sanjay")
+// })
+
+app.listen(7777, () => {
     console.log('Server is started!')
 });
